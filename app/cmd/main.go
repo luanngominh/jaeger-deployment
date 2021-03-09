d59f90d2f449558d037478f8402af74db5276b5d
luan.ngo <luan.ngo@vexere.com>
2021-03-09 15:58:34 +0700

    demo code

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -1,5 +1,74 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Command jaeger is an example program that creates spans
+// and uploads to Jaeger.
 package main
 
-func main() {
+import (
+	"context"
+	"log"
 
+	"go.opentelemetry.io/otel"
+
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/trace/jaeger"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+)
+
+// initTracer creates a new trace provider instance and registers it as global trace provider.
+func initTracer() func() {
+	// Create and install Jaeger export pipeline.
+	flush, err := jaeger.InstallNewPipeline(
+		jaeger.WithAgentEndpoint("localhost:6831"),
+		jaeger.WithProcess(jaeger.Process{
+			ServiceName: "hihi",
+			Tags: []attribute.KeyValue{
+				attribute.String("exporter", "jaeger"),
+				attribute.Float64("float", 312.23),
+			},
+		}),
+		jaeger.WithSDK(&sdktrace.Config{DefaultSampler: sdktrace.AlwaysSample()}),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return flush
+}
+
+func main() {
+	ctx := context.Background()
+
+	flush := initTracer()
+	defer flush()
+
+	tr := otel.Tracer("component-main")
+	ctx, span := tr.Start(ctx, "foo")
+	defer span.End()
+
+	bar(ctx)
+}
+
+func bar(ctx context.Context) {
+	tr := otel.Tracer("component-bar")
+	_, span := tr.Start(ctx, "bar")
+
+	ctr := otel.Tracer("component-c")
+	_, cspan := ctr.Start(ctx, "c")
+	cspan.End()
+
+	defer span.End()
+
+	// Do bar...
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module app
 
 go 1.15
 
-require github.com/lightstep/otel-launcher-go v0.16.1 // indirect
+require (
+	github.com/lightstep/otel-launcher-go v0.16.1 // indirect
+	go.opentelemetry.io/otel v0.18.0
+	go.opentelemetry.io/otel/exporters/trace/jaeger v0.18.0
+	go.opentelemetry.io/otel/sdk v0.18.0
+)
